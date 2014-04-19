4a50a3c7a66d14fbbd286981840fcb3934a6e81f
rich-iannone <riannone@me.com>
2014-04-18 22:37:15 -0700

    Add german-specific transliterations to function

--- R/get.transliterations.R
+++ R/get.transliterations.R
@@ -1,6 +1,15 @@
 get.transliterations <- function(language){
   
-
+  if ("German" %in% language | "german" %in% language |
+        "Deutsch" %in% language | "deutsch" %in% language |
+        "DE" %in% language | "de" %in% language){
+    transliterations_DE <-
+      data.frame(from = c("ß", "Ä", "Ö", "Ü", "ä", "ö", "ü"),
+                 to = c("ss", "Ae", "Oe", "Ue", "ae", "oe", "ue"),
+                 type = rep("DE"), stringsAsFactors = FALSE)
+    
+    return(transliterations_DE)
+  }
 
   # End function
 }
