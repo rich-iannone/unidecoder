9438950939095f8ce55cff81bc446f084cc53e83
rich-iannone <riannone@me.com>
2014-04-18 22:37:43 -0700

    Add french-specific transliterations to function

--- R/get.transliterations.R
+++ R/get.transliterations.R
@@ -10,6 +10,27 @@ get.transliterations <- function(language){
     
     return(transliterations_DE)
   }
+  
+  if ("French" %in% language | "french" %in% language |
+        "Français" %in% language | "français" %in% language |
+        "fr" %in% language | "FR" %in% language){
+    transliterations_FR <-
+      data.frame(from = c("À", "Á", "Â", "Ä", "Æ", "È", "É", "Ê",
+                          "Ë", "Î", "Ï", "Í", "Ì", "Ò", "OŒ", "Ü",
+                          "Ç",
+                          "à", "á", "â", "ä", "æ", "è", "é", "ê",
+                          "ë", "î", "ï", "í", "ì", "ò", "œ", "ü",
+                          "ç"),
+                 to = c("A", "A", "A", "A", "Ae", "E", "E", "E",
+                        "E", "I", "I", "I", "I", "O", "Oe", "U",
+                        "C",
+                        "a", "a", "a", "a", "ae", "e", "e", "e",
+                        "e", "i", "i", "i", "i", "o", "oe", "u",
+                        "c"),
+                 type = rep("FR"), stringsAsFactors = FALSE)
+    
+    return(transliterations_FR)
+  }
 
   # End function
 }
