54a4bf906480411b913aa2c61399f81510f09302
rich-iannone <riannone@me.com>
2014-04-28 17:08:02 -0700

    Reorder the transliteration blocks

--- R/get.transliterations.R
+++ R/get.transliterations.R
@@ -15,25 +15,11 @@ get.transliterations <- function(language){
     
     return(transliterations_CZ)
   }
-  
-  
-  
-  if ("German" %in% language | "german" %in% language |
-        "Deutsch" %in% language | "deutsch" %in% language |
-        "DE" %in% language | "de" %in% language){
 
-    transliterations_DE <-
-      data.frame(from = c("ß", "Ä", "Ö", "Ü", "ä", "ö", "ü"),
-                 to = c("ss", "Ae", "Oe", "Ue", "ae", "oe", "ue"),
-                 type = rep("DE"), stringsAsFactors = FALSE)
-    
-    return(transliterations_DE)
-  }
-  
   if ("French" %in% language | "french" %in% language |
         "Français" %in% language | "français" %in% language |
         "fr" %in% language | "FR" %in% language){
-
+    
     transliterations_FR <-
       data.frame(from = c("À", "Á", "Â", "Ä", "Æ", "È", "É", "Ê",
                           "Ë", "Î", "Ï", "Í", "Ì", "Ò", "OŒ", "Ü",
@@ -50,6 +36,18 @@ get.transliterations <- function(language){
                  type = rep("FR"), stringsAsFactors = FALSE)
     
     return(transliterations_FR)
+  }
+
+  if ("German" %in% language | "german" %in% language |
+        "Deutsch" %in% language | "deutsch" %in% language |
+        "DE" %in% language | "de" %in% language){
+
+    transliterations_DE <-
+      data.frame(from = c("ß", "Ä", "Ö", "Ü", "ä", "ö", "ü"),
+                 to = c("ss", "Ae", "Oe", "Ue", "ae", "oe", "ue"),
+                 type = rep("DE"), stringsAsFactors = FALSE)
+    
+    return(transliterations_DE)
   }
 
   if ("Polish" %in% language | "polish" %in% language |
@@ -90,8 +88,6 @@ get.transliterations <- function(language){
     
     return(transliterations_SL)
   }
-  
-  
   
   if (language == "all"){
     
