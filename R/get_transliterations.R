38ea22e6aa77992c55a72ee622642b950eb076ba
rich-iannone <riannone@me.com>
2015-03-31 00:31:17 -0700

    Add transliterations to SL set

--- R/get_transliterations.R
+++ R/get_transliterations.R
@@ -122,8 +122,10 @@ get_transliterations <- function(language){
         "sl" %in% language | "SL" %in% language){
 
     transliterations_SL <-
-      data.frame(from = c("Č", "č", "É", "é", "Š", "š", "Ž", "ž"),
-                 to = c("C", "c", "E", "e", "S", "s", "Z", "z"),
+      data.frame(from = c("Č", "č", "É", "é", "Í", "í", "Š",
+                          "š", "Ž", "ž"),
+                 to = c("C", "c", "E", "e", "I", "i", "S",
+                        "s", "Z", "z"),
                  type = rep("SL"), stringsAsFactors = FALSE)
     
     return(transliterations_SL)
